ae018d405904395b06c6d74e41a594c892a4c3c7
zhangsan <1030103031@qq.com>
2014-11-20 11:26:57 +0800

    Initial Commit

---
+++ 新浪微博/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新浪微博
+//
+//  Created by apple on 14-11-20.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 新浪微博/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  新浪微博
+//
+//  Created by apple on 14-11-20.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
